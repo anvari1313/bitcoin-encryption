8d47d709e0074ac4f25d2bc669ac1ea148dca889
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-03 18:20:20 +0430

    Adding consts file

---
+++ src/const.h
@@ -0,0 +1,16 @@
+//
+// Created by ahmad on 6/3/18.
+//
+
+#ifndef BITCOIN_ENCRYPTION_CONST_H
+#define BITCOIN_ENCRYPTION_CONST_H
+
+// Definition for padding and parsing
+#define MAX_MESSAGE_SIZE 1024
+#define MAX_PAYLOAD_BLOCK_SIZE_BYTE 56
+#define PAD_BLOCK_SIZE 512
+#define LENGHT_PART_LEN_BIT 64
+
+#define CHAR_SIZE 8
+
+#endif //BITCOIN_ENCRYPTION_CONST_H
