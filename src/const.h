69d26557f0b6882c4a902db11effb1d72f5fdd15
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-03 18:48:11 +0430

    Change segments

--- src/const.h
+++ src/const.h
@@ -5,12 +5,13 @@
 #ifndef BITCOIN_ENCRYPTION_CONST_H
 #define BITCOIN_ENCRYPTION_CONST_H
 
+// General Definitions
+#define CHAR_SIZE_BIT 8
+
 // Definition for padding and parsing
 #define MAX_MESSAGE_SIZE 1024
 #define MAX_PAYLOAD_BLOCK_SIZE_BYTE 56
 #define PAD_BLOCK_SIZE 512
 #define LENGHT_PART_LEN_BIT 64
-
-#define CHAR_SIZE 8
 
 #endif //BITCOIN_ENCRYPTION_CONST_H
