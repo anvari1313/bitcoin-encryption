0ac31b9e232828080fd6486c0043bfbc4a2904b6
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-05 20:40:02 +0430

    Changing the define guard to google code style :D

--- src/expansion/tools.h
+++ src/expansion/tools.h
@@ -2,12 +2,12 @@
 // Created by ahmad on 6/5/18.
 //
 
-#ifndef BITCOIN_ENCRYPTION_TOOLS_H
-#define BITCOIN_ENCRYPTION_TOOLS_H
+#ifndef BITCOIN_ENCRYPTION_EXPANSION_TOOLS_H
+#define BITCOIN_ENCRYPTION_EXPANSION_TOOLS_H
 
 #include "../types.h"
 
 ExpansionBlock rot(unsigned int n, ExpansionBlock x);
 ExpansionBlock shf(unsigned int n, ExpansionBlock x);
 
-#endif //BITCOIN_ENCRYPTION_TOOLS_H
+#endif //BITCOIN_ENCRYPTION_EXPANSION_TOOLS_H
