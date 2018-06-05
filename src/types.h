22d077089d191df3f3cff3e036ed84155e60646b
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-05 14:52:08 +0430

    Adding types header file

---
+++ src/types.h
@@ -0,0 +1,13 @@
+//
+// Created by ahmad on 6/5/18.
+//
+
+#ifndef BITCOIN_ENCRYPTION_TYPES_H
+#define BITCOIN_ENCRYPTION_TYPES_H
+
+#include <bitset>
+#include "const.h"
+
+using MessageBlock =  std::bitset<MAX_MESSAGE_SIZE>;
+
+#endif //BITCOIN_ENCRYPTION_TYPES_H
