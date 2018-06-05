7114d0efd1dfc68504f1378c7c9eb6ace382d4b6
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-05 23:28:05 +0430

    Test for pad-parse phase

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <bitset>
+#include <string>
+#include <vector>
+#include "pad_parse/pad_parse.h"
+#include "bitstream/bitstream.h"
+#include "compression/compression.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string message;
+    cin >> message;
+    auto blocks = pad_parse(message);
+    cout << "-------------------------------------------------------------------------------" << endl;
+    for(auto &block : blocks)
+    {
+        cout << block << endl;
+    }
+
     return 0;
 }
