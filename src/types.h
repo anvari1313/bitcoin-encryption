58052f0f0e63c92977c9da8cf31cc274a8602cd6
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-05 15:27:06 +0430

    Adding type for expansion phase

--- src/types.h
+++ src/types.h
@@ -8,7 +8,11 @@
 #include <bitset>
 #include "const.h"
 
+// Pad-Parse phase types
 using PadParseMessageBlock =  std::bitset<PAD_PARSE_BLOCK_SIZE>;
 using ByteBitSet = std::bitset<CHAR_SIZE_BIT>;
+
+// Expansion phase types
+using ExpansionInputBlock = std::bitset<EXPANSION_INPUT_BLOCK_SIZE>;
 
 #endif //BITCOIN_ENCRYPTION_TYPES_H
