40a3eab1e9ef5de801ba45c7a46e4d0d70d8fc29
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-05 20:34:37 +0430

    Implementing tools functions for compression

--- src/compression/tools.cpp
+++ src/compression/tools.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "tools.h"
+#include "../expansion/tools.h"
+
+ExpansionBlock big_sigma0(ExpansionBlock x)
+{
+    return rot(2, x) ^ rot(13, x) ^ rot(22, x) ^ shf(7, x);
+}
+
+ExpansionBlock big_sigma1(ExpansionBlock x)
+{
+    return rot(6, x) ^ rot(11, x) ^ rot(25, x);
+}
+
+ExpansionBlock big_sigma2(ExpansionBlock x)
+{
+    return rot(2, x) ^ rot(3, x) ^ rot(15, x) ^ shf(5, x);
+}
+
+ExpansionBlock ch(ExpansionBlock x, ExpansionBlock y, ExpansionBlock z)
+{
+    return (x & y) ^ (~y & z) ^ (~x & z);
+}
+
+ExpansionBlock maj(ExpansionBlock x, ExpansionBlock y, ExpansionBlock z)
+{
+    return (x & z) ^ (x & y) ^ (y & z);
+}
--- src/compression/tools.h
+++ src/compression/tools.h
@@ -2,13 +2,16 @@
 // Created by ahmad on 6/5/18.
 //
 
-#ifndef BITCOIN_ENCRYPTION_TOOLS_H
-#define BITCOIN_ENCRYPTION_TOOLS_H
+#ifndef BITCOIN_ENCRYPTION_COMPRESSION_TOOLS_H
+#define BITCOIN_ENCRYPTION_COMPRESSION_TOOLS_H
 
+#include "../types.h"
 
-class tools {
+ExpansionBlock big_sigma0(ExpansionBlock x);
+ExpansionBlock big_sigma1(ExpansionBlock x);
+ExpansionBlock big_sigma2(ExpansionBlock x);
 
-};
+ExpansionBlock ch(ExpansionBlock x, ExpansionBlock y, ExpansionBlock z);
+ExpansionBlock maj(ExpansionBlock x, ExpansionBlock y, ExpansionBlock z);
 
-
-#endif //BITCOIN_ENCRYPTION_TOOLS_H
+#endif //BITCOIN_ENCRYPTION_COMPRESSION_TOOLS_H
