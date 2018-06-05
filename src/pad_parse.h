551b5538552567a2603be5288607164a5ebdc5d8
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-05 14:26:23 +0430

    Adding vectorized function header for pad_parse and add flags

--- src/pad_parse.h
+++ src/pad_parse.h
@@ -7,9 +7,18 @@
 
 #include <bitset>
 #include <string>
+#include <vector>
 #include "const.h"
 
+// Flags for Code
+#define VECTORIZED_PAD_PARSE_IMPLEMENTATION         // Determine the output can be vector or not
+#define PAD_PARSE_LOG_ENABLE                        // Enable logging for this function
 
+
+#ifndef VECTORIZED_PAD_PARSE_IMPLEMENTATION
 std::bitset<MAX_MESSAGE_SIZE> *pad_parse(std::string message, size_t &message_block_count);
+#else
+std::vector<std::bitset<MAX_MESSAGE_SIZE>> pad_parse(std::string message, size_t &message_block_count);
+#endif
 
 #endif //BITCOIN_ENCRYPTION_PADDING_PARSING_H
