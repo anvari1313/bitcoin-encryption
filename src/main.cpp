9951e7f03e0080df75eea08c0061e8e8cfca504c
Ahmad Anvari <ahmad.anv@gmail.com>
2018-06-06 12:37:25 +0430

    Last changes to main function

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,15 @@
 using namespace std;
 
 int main() {
+    bitset<4> a = 18;
+    bitset<4> b = 12;
+    bitset<4> c = 6;
+    bitset<4> d = 11;
+    string result = a.to_string() + b.to_string() + c.to_string() + d.to_string();
+    bitset<16> res(result);
+    cout << "String : " << result << endl;
+    cout << "Bitset : " << res << endl;
+    cout << "ULong  : " << res.to_ulong() << endl;
     string message;
     cin >> message;
     auto blocks = pad_parse(message);
