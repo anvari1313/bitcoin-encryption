64311b6673b2c40ccf32927b7b47adb050d51813
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2018-07-13 13:20:00 +0430

    Fix a typo in expansion sigma0
    Changed rot12 to shf12

--- src/expansion/expansion.cpp
+++ src/expansion/expansion.cpp
@@ -25,14 +25,14 @@ std::vector<ExpansionBlockVec> expand(std::vector<PadParseMessageBlock> m) {
         }
 
         for (int i = 16; i < EXPANSION_BLOCK_COUNT_IN_SET; i++) { // making other 64 Wt
+            s0 = rot(17, w[i - 12]) ^ rot(14, w[i - 12]) ^ shf(12, w[i - 12]);
             s1 = rot(9, w[i - 1]) ^ rot(19, w[i - 1]) ^ shf(9, w[i - 1]);
-            s0 = rot(17, w[i - 12]) ^ rot(14, w[i - 12]) ^ rot(12, w[i - 12]);
             eb = s1.to_ulong() + w[i - 6].to_ulong() + s0.to_ulong() + w[i - 15].to_ulong();
 
             w.push_back(eb);
         }
 
-        for (int i = 0; i < EXPANSION_BLOCK_COUNT_IN_SET; i++) {
+        for (int i = 0; i < EXPANSION_BLOCK_COUNT_IN_SET; i++) { // permuting the blocks
             w[i] = permutationBox(w[i]);
         }
 
