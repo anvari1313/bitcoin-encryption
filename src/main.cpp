b34e3686dd33cfbefc176baac7e878149e1e1bc1
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2018-07-13 18:41:59 +0430

    Implement sha256() function

--- src/main.cpp
+++ src/main.cpp
@@ -5,18 +5,31 @@
 #include "pad_parse/pad_parse.h"
 #include "bitstream/bitstream.h"
 #include "compression/compression.h"
+#include "expansion/tools.h"
+#include "expansion/expansion.h"
 
 using namespace std;
+
+HashBlock sha256(string message);
 
 int main() {
     string message;
     cin >> message;
-    auto blocks = pad_parse(message);
-    cout << "-------------------------------------------------------------------------------" << endl;
-    for(auto &block : blocks)
-    {
-        cout << block << endl;
-    }
+
+    HashBlock firstResult = sha256(message);
+
+    cout << endl << hashBlockToHex(firstResult) << endl;
+
 
     return 0;
+
+}
+
+HashBlock sha256(string message) {
+    auto blocks = pad_parse(message);
+
+    auto ws = expand(blocks);
+
+
+    return compress(ws);
 }
