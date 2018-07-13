c89836d49f375448492560b4b2a01118a8530f69
Ahmad Anvari <ahmad.anv@gmail.com>
2018-07-13 22:42:09 +0430

    Remove unnecsary and problemetic functions

--- src/expansion/tools.h
+++ src/expansion/tools.h
@@ -10,7 +10,7 @@
 ExpansionBlock rot(unsigned int n, ExpansionBlock x);
 ExpansionBlock shf(unsigned int n, ExpansionBlock x);
 ExpansionBlock permutationBox(ExpansionBlock block);
-std::string HashBlockToString(HashBlock block);
-std::string hashBlockToHex(HashBlock block);
-std::string expansionBlockToHex(ExpansionBlock block);
+//std::string HashBlockToString(HashBlock block);
+//std::string hashBlockToHex(HashBlock block);
+//std::string expansionBlockToHex(ExpansionBlock block);
 #endif //BITCOIN_ENCRYPTION_EXPANSION_TOOLS_H
